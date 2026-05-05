a78a8adec1ff7d3fd67400aaaee8a331bdb197c2
Santiago <ssantimendez19@gmail.com>
2026-05-05 10:47:41 -0500

    Laboratio 4 Finalizado

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100, null = False)
+    type = models.CharField(max_length=50, null = False)
+    height = models.FloatField()
+    weight = models.FloatField()
+    
+    def __str__(self):
+        return self.name
 # Create your models here.
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
 from django.template import loader
+from pokedex.models import Pokemon
 
 def index(request):
-    pokemons = ['charmander', 'pikachu', 'squirtle']
+    pokemons = Pokemon.objects.all() #select * from pokedex_pokemon
     template = loader.get_template('index.html')
     return HttpResponse(template.render({'pokemons': pokemons}, request))
 
-def pokemon(request, pokemon):
+def pokemon(request, id: int):
+    pokemon = Pokemon.objects.get(id=id) #select * from pokedex_pokemon where
     template = loader.get_template('display_pokemon.html')
     context = {
         'pokemon': pokemon
